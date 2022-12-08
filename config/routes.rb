ee46ce630a6ff3c9b12ddecd3a63255bec26b3f6
maxkarlan <mkarlan@uchicago.edu>
2022-12-08 20:39:46 +0000

    bare bones sign up page set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
 
+  # User routes
+
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form" })
+  get("/user_sign_out", { :controller => "users", :action => "sign_out" })
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form" })
+
+  post("/verify_creds", { :controller => "users", :action => "authenticate" })
 
 end
